1cf885bb5db225678b17c828a51f0658b16e6270
LexBrill <Lex@LCompu.local>
2017-07-23 02:59:17 -0700

    working on adding script for prolivin score

---
+++ Pro Livin/ProLivinScoreController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pro Livin
+//
+//  Created by Ezra Aigbe on 7/23/17.
+//  Copyright © 2017 Ezra Aigbe. All rights reserved.
+//
+
+import UIKit
+
+class ProLivinScoreController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
