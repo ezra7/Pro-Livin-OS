118b948b2c6c3bb326edca87b2c39f2b869ce0ca
Ezra Aigbe <ezraaigbe7@gmail.com>
2017-07-23 02:41:45 -0700

    started working on login

---
+++ Pro Livin/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  Pro Livin
+//
+//  Created by Ezra Aigbe on 7/23/17.
+//  Copyright © 2017 Ezra Aigbe. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor (colorLiteralRed: <#T##Float#>, green: <#T##Float#>, blue: <#T##Float#>, alpha: <#T##Float#>)
+    }
+}
